2200758c223f1100728b932e8b51d282a93e8931
WasokSriparat <sriparat123@gmail.com>
2021-11-23 16:30:13 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1:number = 5;
+  n2:number = 2;
+  picName:string = "https://images.pexels.com/photos/5635101/pexels-photo-5635101.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
+
+  hi(name:string) {
+    alert("Hi " + name);
+  }
+
 }
